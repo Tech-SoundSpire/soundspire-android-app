1123020fefb8022b5f6b221cd1a5a2c68af01ebd
jishnusrivastava <jkssrs@amazon.com>
2026-09-20 02:01:36 +0530

    feat: bump version for suggestions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
     applicationId = "com.aistudio.soundspire.vsqtyz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 15
-    versionName = "2.4"
+    versionCode = 16
+    versionName = "2.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
